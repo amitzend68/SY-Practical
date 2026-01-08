3459e52bafda48803df9ffbd788b90105f0ea4f9
Amit Zend <amitzend68@gmail.com>
2026-01-08 15:33:29 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Addition {
+    public static void main (String[]args){
+
+         Scanner sc = new Scanner (System.in);
+        
+        System.out.println("Enter first number");
+        int a = sc.nextShort();
+         System.out.println("Enter first number");
+        int b =sc.nextShort();
+
+        int Total = a-b;
+
+        System.out.println(Total);
+
+        
+    }
+}
